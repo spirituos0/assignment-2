0c0de8c4011ab6931c69ff86b6c6068c323eeaac
ivan-puhachov <tiltoslave@gmail.com>
2024-09-26 09:33:23 +0300

    first commit

---
+++ Assign2-1_Vika/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace Assign2_1_Vika
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Info = new System.Windows.Forms.Label();
+            this.txtClassC = new System.Windows.Forms.TextBox();
+            this.txtClassB = new System.Windows.Forms.TextBox();
+            this.txtClassA = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtTotalRevenue = new System.Windows.Forms.TextBox();
+            this.txtRevenueC = new System.Windows.Forms.TextBox();
+            this.txtRevenueB = new System.Windows.Forms.TextBox();
+            this.txtRevenueA = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(237, 354);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(91, 43);
+            this.btnCalc.TabIndex = 0;
+            this.btnCalc.Text = "Calculate Revenue";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(353, 354);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(76, 36);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(453, 354);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(64, 36);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Info);
+            this.groupBox1.Controls.Add(this.txtClassC);
+            this.groupBox1.Controls.Add(this.txtClassB);
+            this.groupBox1.Controls.Add(this.txtClassA);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(51, 89);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(325, 229);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tickets Sold";
+            // 
+            // Info
+            // 
+            this.Info.AutoSize = true;
+            this.Info.Location = new System.Drawing.Point(19, 42);
+            this.Info.Name = "Info";
+            this.Info.Size = new System.Drawing.Size(268, 13);
+            this.Info.TabIndex = 17;
+            this.Info.Text = "Enter the number of tickets sold for each class of seats:";
+            // 
+            // txtClassC
+            // 
+            this.txtClassC.BackColor = System.Drawing.SystemColors.Window;
+            this.txtClassC.Location = new System.Drawing.Point(163, 143);
+            this.txtClassC.Name = "txtClassC";
+            this.txtClassC.Size = new System.Drawing.Size(100, 20);
+            this.txtClassC.TabIndex = 5;
+            // 
+            // txtClassB
+            // 
+            this.txtClassB.BackColor = System.Drawing.SystemColors.Window;
+            this.txtClassB.Location = new System.Drawing.Point(163, 113);
+            this.txtClassB.Name = "txtClassB";
+            this.txtClassB.Size = new System.Drawing.Size(100, 20);
+            this.txtClassB.TabIndex = 4;
+            // 
+            // txtClassA
+            // 
+            this.txtClassA.BackColor = System.Drawing.SystemColors.Window;
+            this.txtClassA.Location = new System.Drawing.Point(163, 83);
+            this.txtClassA.Name = "txtClassA";
+            this.txtClassA.Size = new System.Drawing.Size(100, 20);
+            this.txtClassA.TabIndex = 3;
+            this.txtClassA.TextChanged += new System.EventHandler(this.txtClassA_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(77, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Class C:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(77, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Class B:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(77, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Class A:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtTotalRevenue);
+            this.groupBox2.Controls.Add(this.txtRevenueC);
+            this.groupBox2.Controls.Add(this.txtRevenueB);
+            this.groupBox2.Controls.Add(this.txtRevenueA);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(395, 89);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(310, 229);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Revenue Generated";
+            // 
+            // txtTotalRevenue
+            // 
+            this.txtTotalRevenue.BackColor = System.Drawing.SystemColors.Window;
+            this.txtTotalRevenue.Location = new System.Drawing.Point(146, 182);
+            this.txtTotalRevenue.Name = "txtTotalRevenue";
+            this.txtTotalRevenue.Size = new System.Drawing.Size(100, 20);
+            this.txtTotalRevenue.TabIndex = 9;
+            // 
+            // txtRevenueC
+            // 
+            this.txtRevenueC.BackColor = System.Drawing.SystemColors.Window;
+            this.txtRevenueC.Location = new System.Drawing.Point(146, 143);
+            this.txtRevenueC.Name = "txtRevenueC";
+            this.txtRevenueC.Size = new System.Drawing.Size(100, 20);
+            this.txtRevenueC.TabIndex = 8;
+            // 
+            // txtRevenueB
+            // 
+            this.txtRevenueB.BackColor = System.Drawing.SystemColors.Window;
+            this.txtRevenueB.Location = new System.Drawing.Point(146, 113);
+            this.txtRevenueB.Name = "txtRevenueB";
+            this.txtRevenueB.Size = new System.Drawing.Size(100, 20);
+            this.txtRevenueB.TabIndex = 7;
+            // 
+            // txtRevenueA
+            // 
+            this.txtRevenueA.BackColor = System.Drawing.SystemColors.Window;
+            this.txtRevenueA.Location = new System.Drawing.Point(146, 83);
+            this.txtRevenueA.Name = "txtRevenueA";
+            this.txtRevenueA.Size = new System.Drawing.Size(100, 20);
+            this.txtRevenueA.TabIndex = 6;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(55, 185);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Total:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(55, 146);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Class C:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(55, 116);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Class B:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(55, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Class A:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtClassC;
+        private System.Windows.Forms.TextBox txtClassB;
+        private System.Windows.Forms.TextBox txtClassA;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTotalRevenue;
+        private System.Windows.Forms.TextBox txtRevenueC;
+        private System.Windows.Forms.TextBox txtRevenueB;
+        private System.Windows.Forms.TextBox txtRevenueA;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label Info;
+    }
+}
+
---
+++ Assign2-1_Vika/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assign2_1_Vika
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            const decimal classAPrice = 15m;
+            const decimal classBPrice = 12m;
+            const decimal classCPrice = 9m;
+
+            int ticketsA, ticketsB, ticketsC;
+            decimal revenueA, revenueB, revenueC, totalRevenue;
+
+            if (int.TryParse(txtClassA.Text, out ticketsA) &&
+               int.TryParse(txtClassB.Text, out ticketsB) &&
+               int.TryParse(txtClassC.Text, out ticketsC))
+            {
+                revenueA = ticketsA * classAPrice;
+                revenueB = ticketsB * classBPrice;
+                revenueC = ticketsC * classCPrice;
+
+                totalRevenue = revenueA + revenueB + revenueC;
+
+                txtRevenueA.Text = revenueA.ToString("0.00 €");
+                txtRevenueB.Text = revenueB.ToString("0.00 €");
+                txtRevenueC.Text = revenueC.ToString("0.00 €");
+
+                txtTotalRevenue.Text = totalRevenue.ToString("0.00 €");
+            }
+            else
+            {
+                MessageBox.Show("Please enter valid ticket numbers.");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtClassA.Clear();
+            txtClassB.Clear();
+            txtClassC.Clear();
+
+            txtRevenueA.Clear();
+            txtRevenueB.Clear();
+            txtRevenueC.Clear();
+            txtTotalRevenue.Clear();
+        }
+
+        private void txtClassA_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
